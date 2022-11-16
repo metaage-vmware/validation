34a21bae2dbd66c97d25c0e361140dc681f1a28d
wellxion <wellxion@hotmail.com>
2022-11-16 11:16:27 +0800

    add default index api.

--- src/main/java/com/sysage/validation/ValidController.java
+++ src/main/java/com/sysage/validation/ValidController.java
@@ -1,6 +1,7 @@
 package com.sysage.validation;
 
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,11 @@ import javax.validation.Valid;
 @Validated
 @RestController
 public class ValidController {
+    @GetMapping({"/", ""})
+    public String index() {
+        return "validation server up!";
+    }
+
     @PostMapping("/test")
     public String test(@RequestBody @Valid RequestModel<DataModel> body) {
         return body.getData().getValue();
